acce2fa9d25d3514ebb79c13916718dee135e2bd
fagnerdev2024 <fagnertavaresdemenezes2024@gmail.com>
2025-01-30 20:31:38 -0300

    primeiro commit

---
+++ src/main/java/com/fagnerdev/api_rest_blog/ApiRestBlogApplication.java
@@ -0,0 +1,13 @@
+package com.fagnerdev.api_rest_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fagnerdev/api_rest_blog/ApiRestBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fagnerdev.api_rest_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
