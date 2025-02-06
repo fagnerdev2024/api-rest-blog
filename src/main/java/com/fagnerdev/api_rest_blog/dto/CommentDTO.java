f78cd8d2de9421f9fe9bf456c8f2501ec743b85d
fagnerdev2024 <fagnertavaresdemenezes2024@gmail.com>
2025-02-05 21:58:12 -0300

    alteracao em classes

--- src/main/java/com/fagnerdev/api_rest_blog/dto/CommentDTO.java
+++ src/main/java/com/fagnerdev/api_rest_blog/dto/CommentDTO.java
@@ -1,9 +1,12 @@
 package com.fagnerdev.api_rest_blog.dto;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
 
 public class CommentDTO implements Serializable {
+
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private String text;
